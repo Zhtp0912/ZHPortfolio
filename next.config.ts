959c50b030bef4b047168aa9a705c00129ca1623
Zhtp0912 <154117268+Zhtp0912@users.noreply.github.com>
2025-08-27 03:53:25 +0800

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+// Replace with your actual GitHub repository name
+const repoName = "ZHPortfolio";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true, // Optional, helps to avoid 404s in GitHub Pages for paths like `/about/`
+  output: "export",   // Set the output to export for static generation
 };
 
 export default nextConfig;
